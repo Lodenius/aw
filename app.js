757e2edf3f74431fbef1635239f97e9213e63eb1
Adam <adam.l@live.se>
2023-03-01 10:32:48 +0100

    asd

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const activityBtn = document.getElementById('activity-btn');
 const registerBtn = document.getElementById('register-btn');
 const activityEl = document.getElementById('activity');
 
-let activities = [];
 let participants = [];
 
 // Fetch data from API
@@ -19,6 +18,8 @@ async function fetchData() {
 
 // Get random activity
 function getActivity(data) {
+  let activities = [];
+
   data.forEach(activity => {
     activities.push(activity.name);
   });
@@ -38,14 +39,17 @@ function addParticipant(e) {
   renderParticipantList();
 }
 
+// Render participants to UI
 function renderParticipantList() {
   const participantListEl = document.getElementById('participants-list');
+
   const participantList = participants
-  .map(participant => {
-      return `<p>${participant}</p>`;
-    })
+    .map(participant => `<p>${participant}</p>`)
     .join(' ');
-  participantListEl.innerHTML = participantList;
+  participantListEl.innerHTML = `
+  <h3>Participants</h3>
+  <p>${participantList}</p>
+  `;
 }
 
 // Event listeners
